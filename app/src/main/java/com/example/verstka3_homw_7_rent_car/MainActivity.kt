d43ccdbea84bc60e4e439ed8d5cd6538514c5136
abx551 <gniyazowa352@gmail.com>
2024-02-20 17:59:20 +0600

    Initial commit

---
+++ app/src/main/java/com/example/verstka3_homw_7_rent_car/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.verstka3_homw_7_rent_car
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "verstka3_homw_7_rent_car"
+include(":app")
+ 
